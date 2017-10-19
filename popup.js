7dcc41b2b43163a16b3232da61598dd31e94b1c2
haydenjameslee <haydenjameslee@gmail.com>
2017-10-19 18:56:09 +0700

    remove unneeded code

--- popup.js
+++ popup.js
@@ -1,48 +1,4 @@
 
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
 function insertPrimitive(primitive) {
   var script = 'var scene = document.querySelector("a-scene"); if (scene) { var node = document.createElement("'+primitive+'"); scene.appendChild(node); }';
   // See https://developer.chrome.com/extensions/tabs#method-executeScript.
@@ -55,19 +11,9 @@ function insertPrimitive(primitive) {
   });
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
 document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-    var dropdown = document.getElementById('dropdown');
-    dropdown.addEventListener('change', () => {
-      insertPrimitive(dropdown.value);
-    });
+  var dropdown = document.getElementById('dropdown');
+  dropdown.addEventListener('change', () => {
+    insertPrimitive(dropdown.value);
   });
 });
